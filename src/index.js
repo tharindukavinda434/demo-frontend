bb617956a31722b1a3eeeace5e17c776124dc150
Tharindu Bandara <tharindu.bandara@madmobile.com>
2024-08-19 19:50:09 +0530

    test

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
-//      username   = tharinduk
-//password   = !234!23
+username   = tharinduk
+password   = !234!23
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
